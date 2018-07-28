634d4fb5795835cd29a9e6b9c59bbbd13ad87d4c
Dallin <dallinis@hotmail.com>
2018-07-28 06:45:06 -0700

    ex22

---
+++ cmake-build-debug/CMakeFiles/LearningC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearningC.dir/ex22.c.o"
+  "CMakeFiles/LearningC.dir/ex22_main.c.o"
+  "LearningC.pdb"
+  "LearningC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LearningC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
